2cba5d93c65b7f52744144e12e0fb09ff49c0b31
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-03-18 11:37:27 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import * as getFormattedDate from 'productivitytools.learning.node.date';
-import { useState } from 'react';
+import * as getFormattedDate from "productivitytools.learning.node.date";
+import { useState } from "react";
 
 function App() {
-
   const [date, setDate] = useState(null);
 
   const updateDate = () => {
     debugger;
     setDate(getFormattedDate());
-  }
+  };
 
   return (
     <div>
       <p>hello current date:{getFormattedDate()}</p>
       UpdatedDate: {date}
-      <button onClick={updateDate} >Update date</button>
+      <button onClick={updateDate}>Update date</button>
     </div>
   );
 }
